8f85dbd6f2f2c5c8caba5c014348471f8b281750
Ibansyah <ibansyahdienx7@gmail.com>
2023-02-15 22:19:54 +0700

    update

--- database/migrations/2023_02_15_104833_create_products_table.php
+++ database/migrations/2023_02_15_104833_create_products_table.php
@@ -13,6 +13,7 @@ return new class extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
+            $table->integer('id_toko');
             $table->integer('id_category');
             $table->integer('id_sub_category');
             $table->string('name');
@@ -22,6 +23,7 @@ return new class extends Migration
             $table->integer('discount');
             $table->integer('real_price');
             $table->string('slug');
+            $table->integer('status');
             $table->nullableTimestamps();
         });
     }
